3b639fe888710af80a16ceba1c0f12a8c5e0d080
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-12-26 04:38:58 -0700

    updates

--- src/test.ts
+++ src/test.ts
@@ -1,29 +1,29 @@
 
-// import { OpenFoodFactsApi } from "./api/openFoodFactsApi";
-// import { connectDB } from "./db";
-// import { FoodDatabase } from "./utils/food-database";
-// import { CoachAI } from "./coachAI";
+import { OpenFoodFactsApi } from "./api/openFoodFactsApi";
+import { connectDB } from "./db";
+import { FoodDatabase } from "./utils/food-database";
+import { CoachAI } from "./coachAI";
 
 
-// async function main() {
-//   await  connectDB();
+async function main() {
+  await  connectDB();
 
-//     // const results1 = await FoodDatabase.getFoodMatches(["apple", "banana", "coffee", "string bean", "raspberry"]);
-//     // console.log(results1);
+    // const results1 = await FoodDatabase.getFoodMatches(["apple", "banana", "coffee", "string bean", "raspberry"]);
+    // console.log(results1);
 
-//     // const result = await OpenFoodFactsApi.getAPIFoodMatches(["apple", "banana", "coffee", "string bean", "raspberry"]);
-//     // console.log(result);
+    // const result = await OpenFoodFactsApi.getAPIFoodMatches(["apple", "banana", "coffee", "string bean", "raspberry"]);
+    // console.log(result);
 
-//  const prompt = await   CoachAI.promptPiece([
-//         {name: "bar", quantity: "2"},
-//         {name: "banana", quantity: "2"},
-//         {name: "coffee", quantity: "2", unit: "cup"},
-//         {name: "string bean", quantity: "2"},
-//         {name: "raspberry", quantity: "2"},
-//     ]);
+ const prompt = await   CoachAI.promptPiece("test",[
+        {name: "bar", quantity: "2", estimatedCalories: 200},
+        {name: "banana", quantity: "2", estimatedCalories: 200},
+        {name: "coffee", quantity: "2", unit: "cup", estimatedCalories: 200},
+        {name: "string bean", quantity: "2", estimatedCalories: 200},
+        {name: "raspberry", quantity: "2", estimatedCalories: 200},
+    ]);
 
-//     console.log("prompt\n",prompt.prompt);
-//     console.log(prompt.allMatches);
-// }
+    console.log("prompt\n",prompt.prompt);
+    console.log(prompt.allMatches);
+}
 
-// main();
+main();
