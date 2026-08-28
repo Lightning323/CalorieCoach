76d96a1a5c1cefa6230ee59cc30b4c85e714d854
Lightning323 <sam.p.w524@gmail.com>
2026-08-27 20:29:59 -0600

    bump version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "1.1.0";
+export const APP_VERSION = "1.2.0";
 export const DEFAULT_USERNAME = process.env.DEFAULT_USERNAME ?? "Lightning323";
 
 export function getAppVersion() {
